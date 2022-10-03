9ca5eebc673a3cb292e52313abd167180cb53008
kawenav <111808380+kawenav@users.noreply.github.com>
2022-10-02 15:19:52 -1000

    Completed Parts 1-7

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+//Part 4: Created IBOutlets
+    @IBOutlet var topLabel: UILabel!
+    
+    @IBOutlet var bottomLabel: UILabel!
 
+//Part 6: Declare & Assign two New variables
+    var currentIndex = 0
+    
+    var favoriteFoodsArray = ["Burritos", "Sushi", "Fries", "Adobo", "Curry"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+//Part 5: Set the topLabel's text
+        self.topLabel.text = "My Favorite Foods"
+
+//Part 7: Display item at index [0] upon app launch
+        self.bottomLabel.text = favoriteFoodsArray[currentIndex]
     }
 
+    
 
 }
 
