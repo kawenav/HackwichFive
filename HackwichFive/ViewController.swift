b5220cc3a8fb3489d4b0463158c37c6e7f9b6438
kawenav <111808380+kawenav@users.noreply.github.com>
2022-10-02 15:51:30 -1000

    Completed Problem Set One

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -34,16 +34,16 @@ class ViewController: UIViewController {
 //Part 8: Set up If/Else statement to iterate through each item in array upon button press
     @IBAction func buttonPressed(_ sender: Any) {
         
-        if currentIndex < favoriteFoodsArray.count
+        if currentIndex < favoriteFoodsArray.count //Set up conditional statement & the framework for conditional statement
         {
-            bottomLabel.text = favoriteFoodsArray[currentIndex]
-            buttonLabel.setTitle("Next", for: UIControl.State.normal)
-            currentIndex += 1
+            bottomLabel.text = favoriteFoodsArray[currentIndex] //The text of the bottomLabel will display the currentIndex of the favoriteFoodsArray
+            buttonLabel.setTitle("Next", for: UIControl.State.normal) //Setting the button's title to "next" will indicate to the user to keep pressing                                                           the button
+            currentIndex += 1                                   //Since there are still more elements within the favoriteFoodsArray, the currentIndex                                                       needs to increase by 1
         }
     else
         {
-            print("button has been disabled")
-            (buttonLabel as UIButton).isEnabled=false
+            print("button has been disabled")                   //After all elements of the favoriteFoodsArray has been displayed, the button needs to be                                                   disabled--it needs to come to a stop
+            (buttonLabel as UIButton).isEnabled=false           //This indicates that the action of the buttonLabel has stopped
         }
             
     
